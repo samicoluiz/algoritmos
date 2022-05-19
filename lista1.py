5f0f98c2e01127aac0b5e2c55cfdf496b2fc7a34
Luiz Samico <loouiz@gmail.com>
2022-05-19 20:51:16 -0300

    iniciando

---
+++ lista1.py
@@ -0,0 +1,22 @@
+# Questão 1
+m = int(input('Insira um número inteiro: '))
+n = int(input('insira outro numero inteiro: '))
+soma = 0
+while m < n:
+    print(m)
+    soma += m
+    m += 1
+print(soma)
+
+# Questão 2
+qtdNum = int(input('Insira a quantidade de números da sequência de Fibonacci que deseja saber: '))
+termo0 = qtdNum
+termo1 = 
+for i in range(qtdNum + 1):
+    if qtdNum == 0:
+        print(1)
+    if qtdNum == 1:
+        print(0)
+        print(1)
+    else:
+        
