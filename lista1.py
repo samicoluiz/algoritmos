34374b051ce056b4d44afc31e8e2f4ce264535ec
Luiz Samico <loouiz@gmail.com>
2022-05-22 20:19:09 -0300

    Questão 7 pronta

--- lista1.py
+++ lista1.py
@@ -64,3 +64,49 @@ while contador < expoente:
 print(resultado)
 
 # Questão 7
+while True:
+    print('**********************************************************')
+    print('*             CÁLCULO DE GRANDEZAS ELÉTRICAS             *')
+    print('**********************************************************')
+    print('1. Tensão (em Volt)')
+    print('2. Resistência (em Ohm)')
+    print('3. Corrente (em Ampére)')
+    print('**********************************************************')
+
+    operacao = int(input('Qual grandeza deseja calcular?'))
+
+    if operacao == 1:
+        resistencia = float(input('Insira um valor de resistência (em Ohm)'))
+        corrente = float(input('Insira um valor de corrente (em Ampére)'))
+        print(f'A tensão é {round(resistencia * corrente, 2)}V')
+        continuar = input('Deseja realizar outra operação? [s/n]')
+        if continuar == 's':
+            continue
+        elif continuar == 'n':
+            break
+        else:
+            print('Digite "s" para realizar outra operação ou "n" para sair do programa.')
+    elif operacao == 2:
+        tensao = float(input('Insira um valor de tensão (em Volt)'))
+        corrente = float(input('Insira um valor de corrente (em Ampére)'))
+        print(f'A resistência é {round(tensao / corrente, 2)}\u03A9')
+        continuar = input('Deseja realizar outra operação? [s/n]')
+        if continuar == 's':
+            continue
+        elif continuar == 'n':
+            break
+        else:
+            print('Digite "s" para realizar outra operação ou "n" para sair do programa.')
+    elif operacao == 3:
+        tensao = float(input('Insira um valor de tensão (em Volt)'))
+        resistencia = float(input('Insira um valor de resistência (em Ohm)'))
+        print(f'A corrente é {round(tensao/resistência, 2)}A')
+        continuar = input('Deseja realizar outra operação? [s/n]')
+        if continuar == 's':
+            continue
+        elif continuar == 'n':
+            break
+        else:
+            print('Digite "s" para realizar outra operação ou "n" para sair do programa.')
+    else:
+        print('Número inválido. Digite um número de 1 a 3.')
